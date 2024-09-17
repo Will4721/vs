d89955dee3ccb31ce3f1bffaf0ad72c7ce303b27
Will4721 <williamlehd@gmail.com>
2024-09-17 08:59:45 +0200

    mere jason stuff

--- 04-data/index.js
+++ 04-data/index.js
@@ -8,10 +8,13 @@ function setup(){
 }
 
 function setupMenuStructure(){
+    // select alle class page
     pages = selectAll('.page')
     menuItems = selectAll('.menuitem')
 
     //menu items skal reagere ved at skifte side
+    // lave et variable m, hver gang loop kører, er m lige med 1 indtil der ikke mere. 
+    // m.mousepressed, tyrk på div, mousePressed
     for( m of menuItems ){
         m.mousePressed( function(e) {
             //e.target er selve html div'en 
@@ -33,14 +36,65 @@ function setupMenuStructure(){
 }
 
 function pageOne(){
-    console.log('Side 1 funktionen kaldes')
-    
+    console.log('side 1 funktionen kaldes')
 }
 
 function pageTwo(){
-    //først beder vi fetch hente den lokale fil
-    //Først kalder vi server API'ets endpoint
+
+    //Først beder vi fetch hente den lokale fil
     fetch('./mydata.json')
+    
+
+    //så venter vi på serverens promise, der kommer tilbage med .then()
+    .then(
+        function(response){
+            //lad os tjekke om serverens response er okay
+            console.log(response)
+            //og hvis det er det, beder vi serveren om at give os json resultatet 
+            return response.json()
+        }
+    )
+    //og når DET så kommer tilbage 
+    .then(
+        function (data){
+            //vi har nu en random drink
+            //data.Name, så ploter kun Name pikachu
+            console.log(data)
+            //p5 funktion der laver en ny div
+            let newDiv =createElement('div')
+            //så laver vi en overskrift 
+            let newHeader = createElement('h1', data.Name)
+            //så laver vi et p-element 
+            let newP = createElement('p', data.Description)
+            //nu laver vi en underoverskrift
+            let hairHeader = createElement('h3', 'tidligere hårfarver')
+            //nu skal jeg løbe et array igennem fra json
+            let hairlist = createElement('ul')
+            for(color of data.formerHairColors){
+                hairlist.child(listItem)
+            }
+
+            //til sidst lægger vi de nye elementer ind i den div vi opretter
+            newDiv.child(newP)
+            newDiv.child(newHeader)
+            //tag fat i html element med id = localdata
+            //tøm det
+            select('#localData').child(newDiv)
+         
+            
+
+
+
+
+
+            
+        }
+    )
+}
+
+function pageThree(){
+    //Først kalder vi server API'ets endpoint
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
 
     //så venter vi på serverens promise, der kommer tilbage med .then()
     .then(
@@ -56,36 +110,8 @@ function pageTwo(){
         function (data){
             //vi har nu en random drink
             console.log(data)
-            let newDiv= createElement('div')
-            let newHeader = createElement('h1', data.Name)
-            let newP = createElement('p', data.decription)
-            newDiv.child(newHeader)
-                select('#localData').child(dewDiv)
-            
         }
     )
-}
-
-function pageThree(){
-     //Først kalder vi server API'ets endpoint
-     fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
-
-     //så venter vi på serverens promise, der kommer tilbage med .then()
-     .then(
-         function(response){
-             //lad os tjekke om serverens response er okay
-             console.log(response)
-             //og hvis det er det, beder vi serveren om at give os json resultatet 
-             return response.json()
-         }
-     )
-     //og når DET så komer tilbage 
-     .then(
-         function (data){
-             //vi har nu en random drink
-             console.log(data)
-         }
-     )
 }
 
 function pageFour(){
@@ -109,23 +135,26 @@ function shiftPage(num){
     select("#page" + currentPage).addClass('visible')
     select("#menu" + currentPage).addClass('active')
 
+
+
     if(currentPage == 1) {
         pageOne()
     }
     if(currentPage == 2) {
         pageTwo()
     }
-    if(currentPage == 3) {
+
+    if(currentPage == 2) {
         pageThree()
     }
+
     if(currentPage == 4) {
         pageFour()
     }
+    
 }
 
 function keyPressed(){
     console.log(key)
     shiftPage(key)
 }
-
-
